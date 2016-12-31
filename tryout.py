b2c429834ae113827a6b080dab206064b9f75be0
Leonard Koll <Leonard Koll>
2016-12-31 12:30:47 +0100

    PUB_SUB Tryout

--- tryout.py
+++ tryout.py
@@ -2,25 +2,37 @@ import threading
 import zmq
 import time
 
-def replier (context):
-    senderSocket = context.socket(zmq.REP)
+def server (context):
+    senderSocket = context.socket(zmq.PUB)
     #senderSocket.bind("tcp://*:5555")
     senderSocket.bind("inproc://meinKanal")
-    print(senderSocket.recv())
+    for iA in range (5):
+        for subscriptionID in range (2):
+            senderSocket.send_string(str(subscriptionID) + " Hello" + str(iA))
+            time.sleep(0.5)
 
-def requester (context):
-    recvSocket = context.socket(zmq.REQ)
+def client (context):
+    recvSocket = context.socket(zmq.SUB)
     #recvSocket.connect("tcp://localhost:5555")
     recvSocket.connect("inproc://meinKanal")
-    recvSocket.send_string("Hello World")
+    # recvSocket.subscribe("") # alle Messages
+    recvSocket.subscribe("1")  # nur Messages mit ID 1 (Stringmaching am Anfang)
+    while(True):
+        print(recvSocket.recv())
 
 zmqContext = zmq.Context.instance()
 
-sThread = threading.Thread(target=replier, args=(zmqContext,))
+"""
+Wir starten absichtlich den server zuerst. Der sendet im 0.5 sekunden Takt nachrichten.
+Der client schaltet sich erst nach 1 Sekunde zu, die ersten beiden Hellos verpasst er also.
+"""
+
+sThread = threading.Thread(target=server, args=(zmqContext,))
 sThread.start()
 time.sleep(1)
-rThread = threading.Thread(target=requester, args=(zmqContext,))
+rThread = threading.Thread(target=client, args=(zmqContext,))
 rThread.start()
+
 
 time.sleep(3)
 
